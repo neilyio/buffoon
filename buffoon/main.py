0296da621f56d57e80288d5ab208ca8eee430076
Neil Hansen <neil.hansen.31@gmail.com>
2020-05-11 21:03:37 -0700

    Fix linter errors.

--- buffoon/main.py
+++ buffoon/main.py
@@ -2,8 +2,10 @@ import subprocess
 from pathlib import Path
 from typing import Optional, Callable, Any
 
+
 def default_heading() -> str:
     return "* HeadingTest"
+
 
 def loguru_heading(message: Any) -> str:
     r = message.record
@@ -13,8 +15,10 @@ def loguru_heading(message: Any) -> str:
     time = f"{date}/{clock}"
     return f"* {time} {r['module']}::{r['line']}::{r['function']}"
 
+
 def loguru_content(message: Any) -> str:
     return message.record['message']
+
 
 def which(program: str) -> Optional[str]:
     """
@@ -23,6 +27,7 @@ def which(program: str) -> Optional[str]:
     If not found, returns None.
     """
     import os
+
     def is_exe(fpath):
         pathexts = os.environ["PATHEXT"].split(os.pathsep)
         pathexts.append("")
@@ -45,32 +50,38 @@ def which(program: str) -> Optional[str]:
 
     return None
 
+
 def format_lisp(buffer_name, heading: str, content: str, lisp: str) -> str:
     lisp = lisp.replace("{___PYTHON_PLACEHOLDER_BUFFER___}", buffer_name)
     lisp = lisp.replace("{___PYTHON_PLACEHOLDER_HEADING___}", heading)
     lisp = lisp.replace("{___PYTHON_PLACEHOLDER_CONTENT___}", content)
     return lisp
 
+
 def read_template() -> str:
-    template_path = Path(__file__).parent.parent.joinpath('template.el') 
+    template_path = Path(__file__).parent.parent.joinpath('template.el')
     with open(template_path, "r") as f:
         return f.read()
+
 
 def emacsclient(*args: str) -> None:
     if not which('emacsclient'):
         raise FileNotFoundError("Could not find 'emacsclient' on the $PATH.")
-    result = subprocess.run(['emacsclient'] + list(args), stderr=subprocess.PIPE)
+    result = subprocess.run(['emacsclient'] + list(args),
+                            stderr=subprocess.PIPE)
     if result.returncode != 0:
-        raise ChildProcessError(result.stderr.decode('windows-1252')) 
+        raise ChildProcessError(result.stderr.decode('windows-1252'))
+
 
 def buffoon(message: Any,
-            buffer_name:str = "*Buffoon Python Logs*",
-            heading_parser:Optional[Callable[[str], str]] = None,
-            content_parser:Optional[Callable[[str], str]] = None) -> None:
+            buffer_name: str = "*Buffoon Python Logs*",
+            heading_parser: Optional[Callable[[str], str]] = None,
+            content_parser: Optional[Callable[[str], str]] = None) -> None:
     heading = heading_parser(message) if heading_parser else default_heading()
     content = content_parser(message) if content_parser else ""
     lisp = format_lisp(buffer_name, heading, content, read_template())
     emacsclient('-e', '-u', lisp)
+
 
 def loguru_buffoon(message: Any) -> None:
     buffoon(message,
